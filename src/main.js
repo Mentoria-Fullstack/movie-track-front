c2678081c540ce81befc9d0b0c57063e8181bc24
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-24 16:51:00 -0300

    include router and vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 import './assets/main.css'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
+
+
+
+
+
