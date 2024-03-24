fe48c5bc8bd799d30296c8f2c537177b7fc99192
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-24 16:54:56 -0300

    include tv shows view

---
+++ src/views/TvShowsView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import MediaCard from '@/components/MediaCard.vue';
+import MenuBar from '@/components/MenuBar.vue';
+import SearchBar from '@/components/SearchBar.vue';
+import axios from 'axios'
+import { reactive, onMounted } from 'vue'
+
+const data = reactive({})
+
+onMounted(async () => {
+  const response = await axios.get('http://192.168.0.109:80/api/tvshows')
+  data.value = response.data
+})
+    
+</script>
+<template>
+  <SearchBar/>
+  <div class="bg-[#263537] pb-12 p-5">
+    <div class="" v-for="movie in data.value">
+      <MediaCard 
+        :title="movie.media.title" 
+        :description="movie.media.description" 
+        :seasons="movie.seasons" 
+        :episodes="movie.episodes" 
+        :cover="movie.media.cover"
+      />
+    </div>
+  </div>
+  <MenuBar/>
+</template>
+
