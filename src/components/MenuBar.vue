de317f5e6cbb2a698a72360e7e6f9593c6268352
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-24 16:49:06 -0300

    add menu bar and search bar

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div>
+        <button type="button" ><a href="/">Movies</a></button>
+        <button type="button" ><a href="/tvshows">Tv Shows</a></button>
+        <button type="button" ><a href="/signin">Profile</a></button>
+    </div>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+    div{
+        padding: 0 1rem;
+        display: flex;
+        justify-content: space-between;
+        position: fixed;
+        bottom: 0;
+        background-color: #248588;
+        width: 100%;
+        height: 3rem;
+    }
+    button{
+        color: #FFFFFF;
+    }
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="fixed flex flex-row w-full p-2 bg-black/70">
+    
+    <select id="cars" name="cars" class="bg-white rounded-md mr-2 px-2 ">
+      <option value="{{ maxduration = 100 }}">100</option>
+      <option value="saab">200</option>
+      <option value="mercedes">300</option>
+      <option value="audi">400</option>
+    </select>
+
+    <input v-model="maxduration" type="number"  class=" bg-white rounded-md pl-1 py-0.5 w-full" placeholder="Pesquisar">
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
