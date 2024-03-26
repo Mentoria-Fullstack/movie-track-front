bebb1e16f336de61881f21d0efa1026a1b79618e
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-26 13:48:12 -0300

    search by hour implemented

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,23 +3,54 @@ import MediaCard from '@/components/MediaCard.vue';
 import MenuBar from '@/components/MenuBar.vue';
 import SearchBar from '@/components/SearchBar.vue';
 import axios from 'axios'
-import { reactive, onMounted } from 'vue'
+import { reactive, onMounted, ref } from 'vue'
 
 const data = reactive({})
+const filteredData = reactive({})
+const search_input = ref()
+let searchTerm = ''
+
 onMounted(async () => {
   const response = await axios.get('http://192.168.0.109:80/api/movies')
+  data.value = response.data
+  filteredData.value = response.data
+})
 
-    data.value = response.data
-
+const filterByDuration = () => {
+  if (!searchTerm) {
+    filteredData.value = data.value
+    return
   }
-)
+  const durationFilter = parseInt(searchTerm)
+  filteredData.value = data.value.filter(item => item.duration < durationFilter)
+}
+
+const handleInput = () => {
+  const [hours=0, mins=0] = search_input.value.split(':').map(Number)
+  searchTerm = hours * 60 + mins
+  console.log(searchTerm)
+  filterByDuration()
+}
 
 </script>
 
 <template>
-  <SearchBar/>
-  <div class="bg-[#263537] pb-12 p-5">
-    <div class="" v-for="movie in data.value">
+  <div class="fixed flex flex-row w-full p-2 bg-black/70">
+    
+    <!-- not implemented yet -->
+    <select id="filter" name="filter" class="bg-white rounded-md mr-2 px-2 ">
+      <option value="1">opcao 1</option>
+      <option value="2">opcao 2</option>
+      <option value="3">opcao 3</option>
+      <option value="4">opcao 4</option>
+    </select>
+
+    <input @input="handleInput" v-model="search_input" type="text"  class=" bg-white rounded-md pl-1 py-0.5 w-full" placeholder="Pesquisar">
+  </div>
+  <!-- <SearchBar/> -->
+  <div class="bg-[#263537] py-12 px-5 h-full">
+
+    <div v-for="movie in filteredData.value">
       <MediaCard 
         :title="movie.media.title" 
         :description="movie.media.description" 
@@ -30,5 +61,3 @@ onMounted(async () => {
   </div>
   <MenuBar/>
 </template>
-
-<style scoped></style>
